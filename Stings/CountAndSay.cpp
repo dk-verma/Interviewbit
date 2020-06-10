23494b0ea6a9f7b322538f7df89a9920dc1d6f65
Deepak Kumar Verma <dkvermajbp@gmail.com>
2020-06-11 04:07:30 +0530

    Create CountAndSay.cpp

---
+++ Stings/CountAndSay.cpp
@@ -0,0 +1,25 @@
+//1, 11, 21, 1211, 111221, ...
+//Example: if n = 2, the sequence is 11.
+
+string Solution::countAndSay(int A)
+{
+    string ans1 = "1";
+    if(A==1) 
+        return ans1;
+    string pre = countAndSay(A-1);
+    string ans;
+    for(int i=0; i<pre.size(); i++)
+    {
+        int count =1;
+        if( i==pre.size()-1 || pre[i]!=pre[i+1] )
+            ans+=to_string(count)+pre[i];
+            
+        if(pre[i]==pre[i+1])
+        {
+          while( i<pre.size() && pre[i]==pre[i+1])
+            count++,i++;
+          ans+=to_string(count)+pre[i];
+        }
+    }
+    return ans;
+}
